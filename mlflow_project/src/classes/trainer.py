51f54c39603fd597d35a39dc83d091ae95da37c1
alex47ST3 <67763920+alex47ST3@users.noreply.github.com>
2023-04-30 20:07:48 +0200

    s

--- mlflow_project/src/classes/trainer.py
+++ mlflow_project/src/classes/trainer.py
@@ -1,12 +1,15 @@
 from classes.intermediate_step import IntermediateStep
 
+import mlflow
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
 from sklearn.model_selection import RepeatedStratifiedKFold
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import RandomizedSearchCV
 
 import numpy as np
-import pandas as pd
-import os
+
 
 class Trainer(IntermediateStep):
     def __init__(
@@ -16,7 +19,9 @@ class Trainer(IntermediateStep):
             , date_cols
             , target_variable
             , destination_directory
+            , model
             , random_state
+            , splitter
     ):
 
         super().__init__(name
@@ -24,17 +29,21 @@ class Trainer(IntermediateStep):
                          , date_cols
                          , target_variable
                          , destination_directory)
-        
+        self.model = model
+        self.random_state = random_state
+        self.splitter = splitter
+        self.params = None
 
-def random_grid_search(model, param_distributions, X_train, y_train, seed):
+        
+def random_grid_search(self, param_distributions, seed):
     """
     Perform a random grid search using scikit-learn's RandomizedSearchCV class.
 
     Parameters:
-    - model: a scikit-learn model object
+    - self.model: a scikit-learn self.model object
     - param_distributions: a dictionary containing hyperparameter names and distributions
-    - X_train: training input data
-    - y_train: training output data
+    - Self.X_train: training input data
+    - self.y_train: training output data
     - n_iter: number of parameter settings that are sampled
     - cv: number of cross-validation folds
 
@@ -42,26 +51,25 @@ def random_grid_search(model, param_distributions, X_train, y_train, seed):
     - best_estimator: the best estimator found during the search
     - best_params: the hyperparameters of the best estimator
     """
-    cv = RepeatedStratifiedKFold(n_splits=5, n_repeats=3, random_state=seed)
+    cv = RepeatedStratifiedKFold(n_splits=5, n_repeats=3, random_state=self.random_state)
     # Create a RandomizedSearchCV object
 
     search = RandomizedSearchCV(
-        model, param_distributions, cv=cv, scoring='recall', n_iter=10)
+        self.model, param_distributions, cv=cv, scoring='recall', n_iter=10)
 
     # Fit the RandomizedSearchCV object to the data
-    search.fit(X_train, y_train)
+    search.fit(self.X_train, self.y_train)
 
     # Print the best parameters and score
     print("Best parameters: {}".format(search.best_params_))
     print("Best cross-validation score: {:.2f}".format(search.best_score_))
 
-    # Get the best estimator and its parameters
-    best_estimator = search.best_estimator_
-    best_params = search.best_params_
+    return  search.best_params_
 
-    return best_estimator, best_params
 
-def cross_val_train_predict(model, X_train, y_train, X_test, scoring_metric, seed=SEED):
+
+
+def cross_val_train_predict(self, model, X_train, y_train, X_test, scoring_metric, seed=SEED):
     cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=seed)
     scores = cross_val_score(model, X_train, y_train,
                              scoring=scoring_metric, cv=cv, n_jobs=-1)
@@ -70,3 +78,38 @@ def cross_val_train_predict(model, X_train, y_train, X_test, scoring_metric, see
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
     return y_pred
+
+def set_experiment(self, experiment_name, tracking_uri='http://localhost:5000'):
+    # Set the experiment name and tracking URI
+    mlflow.set_experiment(experiment_name)
+    mlflow.set_tracking_uri(tracking_uri)
+    client = mlflow.tracking.MlflowClient()
+    experiment = mlflow.get_experiment_by_name(experiment_name)
+    run = client.create_run(experiment.experiment_id)
+    print(f"Experiment run_id={run.info.run_id} created in tracking URI={tracking_uri}")
+
+
+def run_experiment(self, experiment_name, model_class, params, splitter, log_model=False):
+    
+    set_experiment(experiment_name)
+    mlflow.sklearn.autolog(log_models=log_model)
+    # Log the evaluation metrics and model parameters in MLflow
+    with mlflow.start_run():
+        model = model_class(**params)  # create a new instance of the model class
+        model.fit(splitter.X_train, splitter.y_train)
+        y_pred = model.predict(splitter.X_test)
+        
+    return model, y_pred
+
+def delete_experiment(experiment_name):
+    client = mlflow.tracking.MlflowClient()
+    experiment = mlflow.get_experiment_by_name(experiment_name)
+
+    # Checks if the experiment exists
+    if experiment is None:
+        print(f"Experiment '{experiment_name}' does not exist.")
+        return
+
+    experiment = mlflow.get_experiment_by_name(experiment_name)
+    client.delete_experiment(experiment.experiment_id)
+           
